e62e7a3c8dd659a9ff0dd1bae5169535671553ac
Burima Evgenyi <cuksmail@gmail.com>
2019-05-15 11:56:47 +0500

    added device_reglament_element migration

---
+++ database/migrations/2019_05_15_065432_device_reglament_element.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DeviceReglamentElement extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('device_reglament_element', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('text');
+            $table->unsignedSmallInteger('dvice_reglament_id');
+            $table->unsignedBigInteger('created_user_id');
+            $table->boolean('is_active')->default(1);
+            $table->timestamps();
+
+            $table->foreign('created_user_id')->references('id')->on('users');
+            $table->foreign('dvice_reglament_id')->references('id')->on('dvice_reglament');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('device_reglament');
+        //
+    }
+}
