6aa0c3448d9d0e79ee188f997cab02380d97b172
Burima Evgenyi <cuksmail@gmail.com>
2019-05-15 13:26:41 +0500

    added object_device_reglament_limitations migration

---
+++ database/migrations/2019_05_15_082057_object_device_reglament_limitations.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ObjectDeviceReglamentLimitations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('object_device_reglament_limitations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('object_device_id');            
+            $table->unsignedBigInteger('object_device_reglament_id');
+            $table->unsignedSmallInteger('device_limitations_id');
+            $table->unsignedBigInteger('created_user_id');
+            $table->string('additional_limitation')->nullable();            
+            $table->boolean('is_active')->default(1);
+            $table->timestamps();
+
+            $table->foreign('created_user_id')->references('id')->on('users');
+            $table->foreign('object_device_id')->references('id')->on('object_devices');
+            $table->foreign('object_device_reglament_id')->references('id')->on('object_device_reglament');
+            $table->foreign('device_limitations_id')->references('id')->on('device_limitations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('object_device_reglament_limitations');
+        //
+    }
+}
